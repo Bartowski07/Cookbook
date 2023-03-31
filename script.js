b07786d8d646e0651eb54b0744de51a8d1a13363
Salvatore <111086457+salvo081@users.noreply.github.com>
2023-03-31 19:15:14 +0200

    navi corrections

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function showError() {
-    alert("Sorry, an error occurred. Please try again later.");
-  }
+  alert("Sorry, an error occurred. Please try again later.");
+}
 
-  function dontFollow() {
-    alert("Don't follow me, I'm lost too.");
-  }
+function dontFollow() {
+  alert("Don't follow me, I'm lost too.");
+}
 
   
